b1dfa973232d7930d29c22a7ad0902e5e5e8dcc9
Mr.Miss <keselekpermen69@gmail.com>
2020-07-20 16:00:00 +0700

    simplified and fix msg spacing
    Signed-off-by: Mr.Miss <keselekpermen69@gmail.com>

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from pyrogram import Client
 select = " "
 
 docs = """Generate your Telegram String Session
+
+P -->> Pyrogram [https://docs.pyrogram.org]
 T -->> Telethon [https://docs.telethon.dev]
-p -->> Pyrogram [https://docs.pyrogram.org]
 """
 
 tutor = """
@@ -18,11 +19,17 @@ tutor = """
 ~ Check your Telegram saved messages section to copy the STRING_SESSION
 """
 
+template = """
+UserBot support: @userbotindo
+            
+<code>STRING_SESSION</code>: <code>{}</code>
+
+⚠️ <b>Please be carefull to pass this value to third parties</b>"""
+
 
 print(docs)
 
 while select != ("p", "t"):
-    time.sleep(0.5)
     select = input("Enter your required client < p / t > : ")
     if select == "t":
         print("""\nTelethon selected\nRunning script...""")
@@ -33,16 +40,14 @@ while select != ("p", "t"):
 
         with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
             session_string = client.session.save()
-            saved_messages_template = """Support: @userbotindo
-            
-        <code>STRING_SESSION</code>: <code>{}</code>
-
-        ⚠️ <i>Please be carefull to pass this value to third parties</i>""".format(session_string)
+            saved_messages_template = "Telethon session" + template.format(session_string)
+            print("\nGenerating String Session...\n")
             client.send_message("me", saved_messages_template, parse_mode="html")
+            time.sleep(1)
             print("Your STRING_SESSION value have been sent to your Telegram Saved Messages")
         break
 
-    if select == "p":
+    elif select == "p":
         print("""\nPyrogram selected.\nRunning script...""")
         time.sleep(1)
         print(tutor)
@@ -50,12 +55,13 @@ while select != ("p", "t"):
         "UserBot", 
         api_id=int(input("Enter API ID: ")),
         api_hash=input("Enter API HASH: ")) as pyrogram:
-            print("Generating String session")
-            saved_messages_template = """Support: @userbotindo
-
-        <code>STRING_SESSION</code>: <code>{}</code>
-
-        ⚠️ <i>Please be carefull to pass this value to third parties</i>""".format(pyrogram.export_session_string())
+            saved_messages_template = "Pyrogram session" + template.format(pyrogram.export_session_string())
+            print("\nGenerating String session...\n")           
             pyrogram.send_message("me", saved_messages_template, parse_mode="html")
+            time.sleep(1) 
             print("Your STRING_SESSION value have been sent to your Telegram Saved Messages")
         break
+    
+    else:
+        print("\nPlease only select P or T\n")
+        time.sleep(1.5)
